f29cf5d5bfde5e73407b309da107e969e6fd6bca
U-MIDWOOD\vladperl <vladperl@LMAP129.levitmedical.com>
2010-11-08 22:25:32 -0500

    prepare-app

---
+++ prepare-app.js
@@ -0,0 +1,70 @@
+var sys = require('sys'),
+fs = require('fs'),
+meryl = require('meryl'),
+path = require('path');
+
+exports.prepareTemplate = function(templateName, opts) {
+   templateName = templateName || '/';
+   templateName = templateName.replace(/\.\./, '.', 'g');
+   if (templateName[templateName.length - 1] === '/') {
+      templateName += 'index';
+   }
+   var templatePath = path.join(process.cwd(),
+      opts.templateDir, templateName + opts.templateExt);
+   var src = null;
+   try {
+      src = fs.readFileSync(templatePath, __encoding);
+   } catch (e) {
+      throw 'template not found';
+   }
+   if (src) {
+      opts.loadTemplateFunc(opts.compileTemplateFunc(src, templateName));
+   }
+};
+
+exports.prepareTemplates = function(opts, onLoad) {
+   fs.readdir(opts.templateDir, function (err, filenames) {
+      if (err) {
+         throw err;
+      }
+      var filesRead = 0, templateName, pattern = new RegExp('\\' + opts.templateExt + '$');
+      filenames.forEach(function (filename) {
+         if (pattern.test(filename)) {
+            fs.readFile(path.join(opts.templateDir, filename), function (err, data) {
+               if (err) {
+                  throw err;
+               }
+               templateName = filename.replace(pattern, '');
+               opts.loadTemplateFunc(opts.compileTemplateFunc(data.toString(), templateName));
+               console.log(templateName + ' template prepared.');
+               filesRead += 1;
+               if (filenames.length === filesRead) {
+                  onLoad();
+               }
+            });
+         }
+      });
+   });
+};
+
+exports.prepareControllers = function (controllerDir, onLoad) {
+   fs.readdir(controllerDir, function (err, filenames) {
+      if (err) {
+         throw err;
+      }
+      var filesRead = 0;
+      filenames.forEach(function (filename) {
+         fs.readFile(path.join(controllerDir, filename), function (err, data) {
+            if (err) {
+               throw err;
+            }
+            eval(data.toString());
+            console.log("'" + filename + "' controller prepared.");
+            filesRead += 1;
+            if (filenames.length === filesRead) {
+               onLoad();
+            }
+         });
+      });
+   });
+};
