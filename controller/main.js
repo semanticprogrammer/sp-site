6dd43c807ea91d0857f95675ad630d43ce3437cf
U-MIDWOOD\vladperl <vladperl@LMAP129.levitmedical.com>
2010-11-15 15:28:07 -0500

    fixed main.js

--- controller/main.js
+++ controller/main.js
@@ -1,63 +1,21 @@
 with (meryl) {
-
+   var sys = require('sys');
    get('/', function(req, resp) {
       resp.redirect('/main');
    });
   
-   var currentURL;
-   var data = {
-      currentPage: function(chunk, context) {
-         return (context.current().name.toUpperCase() == currentURL.toUpperCase()) ? "selected" : "";
-      },
-      pages: [
-      {
-         name: "Main",
-         url: "main"
-      },
-      {
-         name: "Technology", 
-         url: "technology"
-      },
-      {
-         name: "Production", 
-         url: "production-css_framework.xhtml"
-      },
-      {
-         name: "Research Lab", 
-         url: "research.xhtml"
-      },
-      {
-         name: "Community", 
-         url: "community.xhtml"
-      },
-      {
-         name: "Market Place", 
-         url: "market.xhtml"
-      }
-      ],
-      posts: [{
-         key:1, 
-         title:"post 1", 
-         date: "01/19/10"
-      },{
-         key:2, 
-         title:"post 2", 
-         date: "01/14/10"
-      },{
-         key:3, 
-         title:"post 3",
-         date: "04/2/10"
-      }]
-   };
+   get('/*', function(req, resp) {
+      var action = null;
+      options.dataStore.currentURL = req.url.substr(1);
 
-   get('/main', function(req, resp) {
-      currentURL = req.url.substr(1);
-      resp.renderTemplate('main', data);
-   });
-  
-   get('/technology', function(req, resp) {
-      currentURL = req.url.substr(1);
-      resp.renderTemplate('technology-web_site', data);
+      options.dataStore.navigation.forEach(function(element, index, array) {
+         if (element.url == options.dataStore.currentURL) {
+//            action = options.actionStore.navigation[options.dataStore.currentURL];
+            action = element;
+         }
+      });
+      if (action != null) {
+         resp.renderTemplate(action.template, options.dataStore); 
+      }
    });
    }
-
