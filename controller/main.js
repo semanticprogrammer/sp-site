7b8addd1b3bece7ded9849c2d1d3a2f015953a2a
U-MIDWOOD\vladperl <vladperl@LMAP129.levitmedical.com>
2010-11-24 21:58:41 -0500

    started CRUD

--- controller/main.js
+++ controller/main.js
@@ -1,16 +1,18 @@
 with (meryl) {
-   var sys = require('sys');
+   var sys = require('sys'),
+   qs = require('querystring');
+
    get('/', function(req, resp) {
       resp.redirect('/main');
    });
   
    get('/*', function(req, resp) {
       var action = null;
-      options.dataStore.currentURL = req.url.substr(1);
+      options.dataStore.currentURL = req.url;
 
       options.dataStore.navigation.forEach(function(element, index, array) {
          if (element.url == options.dataStore.currentURL) {
-//            action = options.actionStore.navigation[options.dataStore.currentURL];
+            //            action = options.actionStore.navigation[options.dataStore.currentURL];
             action = element;
          }
       });
@@ -18,4 +20,37 @@ with (meryl) {
          resp.renderTemplate(action.template, options.dataStore); 
       }
    });
-   }
+
+//   get('/posts', function(req, resp) {
+//   });
+
+   get('/posts/add', function(req, resp) {
+   });
+
+   post('/posts', function(req, resp) {
+      var postdataAsObject = qs.parse(req.postdata.toString());
+      if (postdataAsObject && postdataAsObject.post) {
+         twinkles.push(postdataAsObject.wink);
+      }
+      resp.redirect('/posts');
+   });
+
+   get('/posts/{id}', function(req, resp) {
+   });
+
+   get('/posts/{id}/edit', function(req, resp) {
+   });
+
+   put('/posts/{id}', function(req, resp) {
+   });
+
+   meryl['delete']('/posts/{id}', function(req, resp) {
+      sys.puts("req.params.id:" + req.params.id);
+      options.dataStore.posts.forEach(function(element, index, array) {
+         if (element.key == req.params.id) {
+            sys.puts("delete element = " + element.key);
+            delete element;
+         }
+      });
+   });
+}
--- data/data.js
+++ data/data.js
@@ -12,54 +12,80 @@ opts.dataStore = {
    },
    navigation: [
       {
-         url: "main",
+         url: '/main',
+         name: 'Main',
+         template: 'main',
+         topMenu: 'yes'
+      },
+      {
+         url: '/technology',
+         name: 'Technology',
+         template: 'technology_web_site',
+         topMenu: 'yes'
+      },
+      {
+         url: '/mydata',
+         name: 'My Data',
+         template: 'my_data',
+         topMenu: 'yes'
+      },
+      {
+         url: '/posts',
+         name: 'Posts',
+         template: 'posts'
+      }
+   ],
+   myData: [
+      {
+         url: '/navigation',
+         name: 'Navigation'
+      },
+      {
+         url: '/mydata',
+         name: 'My Data'
+      },
+      {
+         url: '/posts',
+         name: 'Posts'
+      }],
+   pages: [
+      {
          name: "Main",
+         url: "main",
          template: "main"
       },
       {
-         url: "technology",
          name: "Technology",
-         template: "technology_web_site"
+         url: "technology"
+      },
+      {
+         name: "Production",
+         url: "production-css_framework.xhtml"
+      },
+      {
+         name: "Research Lab",
+         url: "research.xhtml"
+      },
+      {
+         name: "Community",
+         url: "community.xhtml"
+      },
+      {
+         name: "Market Place",
+         url: "market.xhtml"
       }
    ],
-   pages: [
-   {
-      name: "Main",
-      url: "main",
-      template: "main"
-   },
-   {
-      name: "Technology",
-      url: "technology"
-   },
-   {
-      name: "Production",
-      url: "production-css_framework.xhtml"
-   },
-   {
-      name: "Research Lab",
-      url: "research.xhtml"
-   },
-   {
-      name: "Community",
-      url: "community.xhtml"
-   },
-   {
-      name: "Market Place",
-      url: "market.xhtml"
-   }
-   ],
    posts: [{
-      key:1,
-      title:"post 1",
-      date: "01/19/10"
-   },{
-      key:2,
-      title:"post 2",
-      date: "01/14/10"
-   },{
-      key:3,
-      title:"post 3",
-      date: "04/2/10"
-   }]
+         key:1,
+         title:"post 1",
+         date: "01/19/10"
+      },{
+         key:2,
+         title:"post 2",
+         date: "01/14/10"
+      },{
+         key:3,
+         title:"post 3",
+         date: "04/2/10"
+      }]
 };
